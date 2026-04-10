69e98debf1d8b736e8907166e7aa94e29b73f9a3
twniazi <fattia923@gmail.com>
2026-04-10 00:19:01 -0700

    Initial commit - Dice Game

---
+++ src/components/RoleDice.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const RoleDice = ({ roleDice, currentDice }) => {
+  return (
+    <DiceContainer>
+      <div className="dice" onClick={roleDice}>
+        <img src={`/images/dice/dice_${currentDice}.png`} alt="dice 1" />
+      </div>
+      <p>Click on Dice to roll</p>
+    </DiceContainer>
+  );
+};
+
+export default RoleDice;
+
+const DiceContainer = styled.div`
+  margin-top: 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .dice {
+    cursor: pointer;
+  }
+
+  p {
+    font-size: 24px;
+  }
+`;
+
+RoleDice.propTypes = {
+  roleDice: PropTypes.func.isRequired,
+  currentDice: PropTypes.number.isRequired,
+};
---
+++ src/components/TotalScore.jsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const TotalScore = ({ score }) => {
+  return (
+    <ScoreContainer>
+      <h1>{score}</h1>
+      <p>Total Score</p>
+    </ScoreContainer>
+  );
+};
+
+export default TotalScore;
+
+const ScoreContainer = styled.div`
+  max-width: 200px;
+  text-align: center;
+  h1 {
+    font-size: 100px;
+    line-height: 100px;
+  }
+  p {
+    font-size: 24px;
+    font-weight: 500px;
+  }
+`;
+
+TotalScore.propTypes = {
+  score: PropTypes.number.isRequired,
+};
